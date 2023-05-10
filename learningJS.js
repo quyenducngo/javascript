e3d8f551052ac5f9fa59aef6c546ae33464e8b45
quyenducngo <quyenducngo@gmail.com>
2023-05-09 21:44:12 -0700

    may 9th, 2023 baby commit

--- learningJS.js
+++ learningJS.js
@@ -1,9 +1,4 @@
-// Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
+let myVar = 11;
 
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+// Only change code below this line
+myVar = myVar - 1;
